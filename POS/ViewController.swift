f0882945e12d44a11dff6902100e9eb905723397
Ahnz <jihoon4680@gmail.com>
2023-10-29 13:16:43 +0900

    2023.10.29 jh.ahn 프로젝트 생성

---
+++ POS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POS
+//
+//  Created by Jihoon on 2023/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
