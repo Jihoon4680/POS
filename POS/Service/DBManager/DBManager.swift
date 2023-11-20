0d8270fe7f2a0876bc932d24e42e8540c3a07fc5
Ahnz <jihoon4680@gmail.com>
2023-11-21 08:26:33 +0900

    테이블 수 세팅 설정

--- POS/Service/DBManager/DBManager.swift
+++ POS/Service/DBManager/DBManager.swift
@@ -15,4 +15,26 @@ class DBManager {
     
     let realm = try! Realm()
     
+    //MARK: 테이블 수 설정
+    /**
+     Description : 테이블 수 설정
+     Date : 2023 . 11 . 21
+     author :  JH
+     Parameter : num ( Int ) // 설정하고 싶은 테이블 수
+     Return :
+     */
+    func setTableNum(num : Int){
+        
+    }
+    //MARK: 테이블 수 가져오기
+    /**
+     Description : 저장된 테이블 수 가져오기
+     Date : 2023 . 11 . 21
+     author :  JH
+     Return : Num (Int)
+     */
+    func getTableNum() -> Int{
+        let num = 0
+        return num
+    }
 }
