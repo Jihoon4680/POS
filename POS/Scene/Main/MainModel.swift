27f06bd73a1af89731e9ab4ad2f8b128fb1437a9
jh.ahn <jh.ahn@infovine.co.kr>
2023-11-23 18:58:57 +0900

    MainModel

--- POS/Scene/Main/MainModel.swift
+++ POS/Scene/Main/MainModel.swift
@@ -14,9 +14,20 @@ let orderOptionStr = ["테이블", "주문내역", "설정"]
  2. 현재 테이블 상황 ( 테이블,메뉴 ( 수량, 이름) )
  */
 
-struct OrderList {
-    let table_no : String // 테이블 번호
-    var menuList : MenuList // 주문한 메뉴 목록
-    let totalMenuCount : Int // 총 주문 개수
-    let totalPrice : Int // 총 주문 금액
+struct MainModel {
+    let tableInfo : TableInfo
+}
+
+struct TableInfo { // 테이블 번호와 주문 정보
+    let tableNum : Int
+    let orderInfo : OrderInfo
+}
+struct OrderInfo {
+    var orderMenuList : [OrderMenu] // 메뉴이름과 주문 수량
+    var totalCount : Int // 총 주문 갯수
+    var totalPrice : Int // 총 결제 가격
+}
+struct OrderMenu {
+    let menuName : String
+    var menuCount : Int
 }
---
+++ POS/Service/DBManager/DBModels/CurrentSalesList.swift
@@ -0,0 +1,8 @@
+//
+//  CurrentSalesList.swift
+//  POS
+//
+//  Created by Jihoon on 11/23/23.
+//
+
+import Foundation
